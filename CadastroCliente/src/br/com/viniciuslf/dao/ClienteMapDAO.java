4d61d5b2ea86e154800210a761dd7c52559079fa
Vinícius <63385115+Viniciuslf@users.noreply.github.com>
2023-07-20 18:28:00 -0300

    Update ClienteMapDAO.java

--- CadastroCliente/src/br/com/viniciuslf/dao/ClienteMapDAO.java
+++ CadastroCliente/src/br/com/viniciuslf/dao/ClienteMapDAO.java
@@ -6,9 +6,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author rodrigo.pires
- */
 public class ClienteMapDAO implements IClienteDAO {
 
     private Map<Long, Cliente> map;
